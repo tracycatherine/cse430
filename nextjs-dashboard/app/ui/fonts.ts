6c8f3583f52c13e3c64eed39d9b3ba181d8fa834
tracycatherine <tracynalubwama5@gmail.com>
2025-12-03 01:42:43 +0300

    week2

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// app/ui/fonts.ts
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
